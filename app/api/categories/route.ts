e763919f037452e36512b09dc19a64eb1e13fed1
Abass Dev <abassthedev@gmail.com>
2024-12-07 11:36:58 +0000

    fix categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,14 +1,10 @@
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
-import { convertPrismaItem } from '@/lib/utils'
 
 export async function GET() {
   try {
     const categories = await prisma.menuCategory.findMany()
-
-    const categoriesItems = categories.map(convertPrismaItem)
-
-    return NextResponse.json(categoriesItems)
+    return NextResponse.json(categories)
   } catch (error) {
     console.error('Error fetching categories:', error)
     return NextResponse.json({ error: 'Error fetching categories' }, { status: 500 })
